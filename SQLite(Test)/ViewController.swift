70f010ac731dc1e815c893a7ef7abb663a5325d4
Nisha <way2nisha.poddar@gmail.com>
2019-07-29 16:55:37 +0530

    Initial commit

--- SQLite(Test)/ViewController.swift
+++ SQLite(Test)/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("abc")
         DBManager.shared.createDatabase()
         DBManager.shared.createTable()
         DBManager.shared.insertInTable()
